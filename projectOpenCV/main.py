8faf35d3ca90b360ba0b8913d6a40778643e86ee
[karthik] <[lcs2022026@iiitl.ac.in]>
2023-04-23 22:01:50 +0530

    the following code sends the body coordinates to the local server

--- projectOpenCV/main.py
+++ projectOpenCV/main.py
@@ -14,3 +14,9 @@ h, w = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_W
 
 cap.release()
 cv2.destroyAllWindows()
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverAddressPort = ("127.0.0.1", 5052)
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        success, image = cap.read()
