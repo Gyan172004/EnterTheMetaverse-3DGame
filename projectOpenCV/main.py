2c3f7373df2e4e8fcb843ca10cac6f827de633f6
Shikhar02347893 <rajshikhar2004@gmail.com>
2023-04-23 22:10:52 +0530

    this code takes each frame from camera then processes coordinates

--- projectOpenCV/main.py
+++ projectOpenCV/main.py
@@ -20,3 +20,11 @@ serverAddressPort = ("127.0.0.1", 5052)
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
         success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
+
+        image = cv2.flip(image, 1)
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = pose.process(image_rgb)
+        
