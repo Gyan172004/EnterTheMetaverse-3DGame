f0d7c89bd9c4b6dbfe0f64f744d131252851ff36
Sanskar Sahu <lci2022043@iiitl.ac.in>
2023-04-24 00:27:09 +0530

    making 2D arrays containing coordinates

--- projectOpenCV/main.py
+++ projectOpenCV/main.py
@@ -26,8 +26,13 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         image = cv2.flip(image, 1)
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = pose.process(image_rgb)
+        if results.pose_landmarks:
+            data = []
+            for lm in results.pose_landmarks.landmark:
+                data.extend([lm.x * w, (1 - lm.y) * h, lm.z * w])
 
-        mp_drawing.draw_landmarks(
+            sock.sendto(str.encode(str(data)), serverAddressPort)
+            mp_drawing.draw_landmarks(
                 image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
         cv2.imshow('MediaPipe Pose', image)
